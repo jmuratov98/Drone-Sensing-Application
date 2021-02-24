d21b5f7307020ca0b7c4333f82f4e5e186d757db
Jonathan <GHJon@github.com>
2021-02-24 18:27:36 -0500

    Added CSV file copying to USB

--- autom/serialconn.py
+++ autom/serialconn.py
@@ -1,11 +1,12 @@
 import serial, string, time
+import shutil
 
 i = 0 # Initializer of amount of data to be collected
 ser = serial.Serial('/dev/ttyS0', baudrate = 9600) # Serial port connection (subject to change on RPI)
 #print(ser.name)
 ser.write('\r') # Exit Low Power Mode (can be any key)
 time.sleep(1)
-text_file = open("test.csv", "w") # Open Text File with write permissions
+text_file = open("testing.csv", "rw") # Open Text File with write permissions
 
 while i < 7:        # Amount of Times to read output (TTC - Time To Collect before moving GPS Location)
  #   print "SN            PPB  T  RH  ADCR   TR     RHR   D   H   M   S" #For Readability ONLY
@@ -20,4 +21,6 @@ while i < 7:        # Amount of Times to read output (TTC - Time To Collect befo
 
 #ser.write('s') # Enter Low-power standby mode
 text_file.close()
-ser.close() # Close Serial port
+ser.close() # Close Serial port 
+
+shutil.copy(src="/bin/testing.csv", dst="/media/pi/USB/testing.csv") #copies file from RPI to USB Drive

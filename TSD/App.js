8abead9e7bf05b5b5146b6d57589111d21b550a5
Joseph Muratov <jmuratov98@gmx.com>
2021-03-05 14:26:37 -0500

    Started work for mobile app

---
+++ TSD/App.js
@@ -0,0 +1,63 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+import MapView, { Heatmap, PROVIDER_GOOGLE } from 'react-native-maps';
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: 400,
+    width: 400,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+        style={styles.map}
+        initialregion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}>
+        <Heatmap
+          points={[
+            { latitude: 37.782, longitude: -122.447, weight: Math.random() },
+            { latitude: 37.782, longitude: -122.445, weight: Math.random() },
+            { latitude: 37.782, longitude: -122.443, weight: Math.random() },
+            { latitude: 37.782, longitude: -122.441, weight: Math.random() },
+            { latitude: 37.782, longitude: -122.439, weight: Math.random() },
+            { latitude: 37.782, longitude: -122.437, weight: Math.random() },
+            { latitude: 37.782, longitude: -122.435, weight: Math.random() },
+            { latitude: 37.785, longitude: -122.447, weight: Math.random() },
+            { latitude: 37.785, longitude: -122.445, weight: Math.random() },
+            { latitude: 37.785, longitude: -122.443, weight: Math.random() },
+            { latitude: 37.785, longitude: -122.441, weight: Math.random() },
+            { latitude: 37.785, longitude: -122.439, weight: Math.random() },
+            { latitude: 37.785, longitude: -122.437, weight: Math.random() },
+            { latitude: 37.785, longitude: -122.435, weight: Math.random() },
+          ]}
+        />
+      </MapView>
+      {/* <Text>Hello World</Text> */}
+    </View>
+  );
+};
+
+export default App;

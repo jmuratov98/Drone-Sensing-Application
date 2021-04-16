de00cf857ab3e8720b8e9b4aec329e2fbf90a525
Jonathan Rodriguez <jonrodriguez016@gmail.com>
2021-04-16 00:27:24 -0400

    Fixed syntax + comments

--- autom/Navio2/Python/SensorGPS.py
+++ autom/Navio2/Python/SensorGPS.py
@@ -5,22 +5,25 @@ import csv
 import os, time, sys
 import serial, string
 
-ser = serial.Serial('/dev/ttyUSB0', baudrate = 9600, timeout=2) # Serial port connection (subject to change on RPI)
-#print(ser.name)
-usbmountpath = "/media/usb"
+ser = serial.Serial('/dev/ttyUSB0', baudrate=9600, timeout=2) # Serial port connection (subject to change on RPI)
+
+usbmountpath = "/media/usb" #For USB not Plugged in, close Sensor Connection and Script
 if os.path.ismount(usbmountpath) == False:
 	ser.close()
 	sys.exit("Closed NO2 Sensor Connection. Reboot System with USB connected") # USB not mounted
 
-text_file = open("TestData.csv", 'w+')
+text_file = open("TestData.csv", 'w+') #Open new CSV File if not available
 csvwriter = csv.writer(text_file)
 csvwriter.writerow(['Longitude','Latitude','Height','hMSL','hACC','vACC','SN','PPB','T','RH','ADCR','TR','RHR','D','H','M','S'])
+
 if __name__ == "__main__":
 
 	ubl = navio.ublox.UBlox("spi:0.0", baudrate=5000000, timeout=2)
+	
 	ubl.configure_poll_port()
 	ubl.configure_poll(navio.ublox.CLASS_CFG, navio.ublox.MSG_CFG_USB)
 	#ubl.configure_poll(navio.ublox.CLASS_MON, navio.ublox.MSG_MON_HW)
+	
 	ubl.configure_port(port=navio.ublox.PORT_SERIAL1, inMask=1, outMask=0)
 	ubl.configure_port(port=navio.ublox.PORT_USB, inMask=1, outMask=1)
 	ubl.configure_port(port=navio.ublox.PORT_SERIAL2, inMask=1, outMask=0)
@@ -29,8 +32,10 @@ if __name__ == "__main__":
 	ubl.configure_poll_port(navio.ublox.PORT_SERIAL2)
 	ubl.configure_poll_port(navio.ublox.PORT_USB)
 	ubl.configure_solution_rate(rate_ms=1000)
+	
 	ubl.set_preferred_dynamic_model(None)
 	ubl.set_preferred_usePPP(None)
+	
 	ubl.configure_message_rate(navio.ublox.CLASS_NAV, navio.ublox.MSG_NAV_POSLLH, 1)
 	ubl.configure_message_rate(navio.ublox.CLASS_NAV, navio.ublox.MSG_NAV_PVT, 1)
 	ubl.configure_message_rate(navio.ublox.CLASS_NAV, navio.ublox.MSG_NAV_STATUS, 1)
@@ -50,9 +55,11 @@ if __name__ == "__main__":
 	
 	adc = navio.adc.ADC()
 	voltread = adc.read(2)/1000*11.3 #Initial Voltage value from Power Module (ADC0)
+	
 	start_time = time.time() #in seconds
 	elapsedtime = 0 #in seconds
-	ser.write('\r')
+	
+	ser.write('\r') #for turning on sensor
 	
 	while voltread >= 12.7 and elapsedtime <= 60: #Continue reading GPS+Sens until voltread < 12.7V or flight time > 10 min
 		while True:
@@ -68,7 +75,7 @@ if __name__ == "__main__":
 					#print(empty)
 					#break
 				#print(msg.name())		
-				if msg.name() == "NAV_POSLLH":
+				if msg.name() == "NAV_POSLLH": #state to gether Lat, Long, etc.
 					outstr = str(msg) .split(",")[1:]
 					outstr = "".join(outstr)
 					strings = [" Latitude=", " height=", " hMSL=", " vAcc=", " hAcc="] 
@@ -84,12 +91,12 @@ if __name__ == "__main__":
 					fullstr = outstr + ", " + output
 					text_file.write(fullstr)
 					print(fullstr)
-					break #Break out of Try-Except if no errors are found
+					break #Break out of Try-Except if no errors are found (run outer while loop)
 			except serial.SerialException:
 				print("No Data Received from NO2 Sensor, Try Again...")
 		voltread = adc.read(2)/1000*11.3
 		elapsedtime = time.time() - start_time
-		print(elapsedtime, 'Seconds. Voltage Reading', voltread)
+		print(elapsedtime, " Seconds Elapsed. Voltage Reading is: ", voltread)
 text_file.close()
 ser.close() # Close Serial port 
 os.system('sudo cp -f /home/pi/Drone-Sensing-Application/autom/Navio2/Python/TestData.csv /media/usb/GPSensData.csv')

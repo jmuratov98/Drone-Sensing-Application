b983faca9d304787c4265c78da1c83e99a15aadb
Joseph Muratov <jmuratov98@gmx.com>
2021-04-23 15:29:57 -0400

    Added a 3D scatter plot

--- desktop-app/src/render/map.js
+++ desktop-app/src/render/map.js
@@ -4,11 +4,16 @@ const { ipcRenderer } = require('electron');
 // Global variables
 let map;
 let heatmap;
+
 let data;
+
 let rangeDOM;
 let height;
+
 let currentHeightSpan;
 let currentPPMSpan;
+
+let graph3DDom;
 
 // Events
 ipcRenderer.on('send-csv-file', async (_, file) => {
@@ -19,9 +24,26 @@ ipcRenderer.on('send-csv-file', async (_, file) => {
 currentHeightSpan = document.getElementById('current-height');
 currentPPMSpan = document.getElementById('current-ppm');
 rangeDOM = document.querySelector('input[type=range]');
+graph3DDom = document.getElementById('3d-graph')
+
 rangeDOM.addEventListener('change', () => {
     height = rangeDOM.value;
     updateHeatMap();
+});
+
+graph3DDom.addEventListener('click', () => {
+    const map = document.getElementById('map');
+    const graph = document.getElementById('graph');
+
+    if (graph3DDom.checked) {
+        map.style.display = 'none';
+        graph.style.display = 'block';
+        update3DGraph();
+    } else {
+        map.style.display = 'block';
+        graph.style.display = 'none';
+        updateHeatMap();
+    }
 });
 
 
@@ -44,7 +66,7 @@ function generateHeatMap() {
 }
 
 function updateHeatMap() {
-    if(!data) return; // Guard clause to make sure undefined data wont go through
+    if (!data) return; // Guard clause to make sure undefined data wont go through
 
     const filteredData = data.filter(dp => { let temp = dp.Height - height; return (temp >= 0 && temp < 10) });
 
@@ -59,4 +81,46 @@ function updateHeatMap() {
         ));
     heatmap.setData(heatmapData);
     heatmap.setMap(map);
+}
+
+function update3DGraph() {
+    const gData = data.reduce((obj, dp) => {
+        obj.x.push(parseFloat(dp['Latitude']));
+        obj.y.push(parseFloat(dp['Longitude']));
+        obj.z.push(dp['PPB'] / 1000);
+        return obj;
+    }, { x: [], y: [], z: [] });
+    console.log(gData);
+    
+
+    const graphData = [{
+        x: gData.x,
+        y: gData.y,
+        z: gData.z,
+        type: 'scatter3d',
+        contours: {
+            z: {
+                show: true,
+                usecolormap: true,
+                highlightcolor: "#42f462",
+                project: { z: true }
+            }
+        }
+    }];
+
+    const layout = {
+        title: 'Mt Bruno Elevation With Projected Contours',
+        scene: { camera: { eye: { x: 1.87, y: 0.88, z: -0.64 } } },
+        autosize: false,
+        width: 500,
+        height: 500,
+        margin: {
+            l: 65,
+            r: 50,
+            b: 65,
+            t: 90,
+        }
+    };
+
+    Plotly.newPlot('graph', graphData, layout);
 }
